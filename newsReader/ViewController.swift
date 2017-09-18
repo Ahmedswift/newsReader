88e2960abd975a33cce540ffcc8a2fb28f5b7c2d
Ahmad Ahrbi <ahmadahrbi@ahmads-mbp.lan>
2017-09-18 13:08:00 +0300

    Initial Commit

---
+++ newsReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newsReader
+//
+//  Created by Ahmad Ahrbi on 12/27/1438 AH.
+//  Copyright © 1438 Ahmad Ahrbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
